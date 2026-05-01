67e9f295ec651b52ddb71fbd6fbda27ce507df94
Mustafa <apabilim@yahoo.com>
2026-05-01 20:35:23 +0300

    Set session cookie to expire on browser close

--- server.js
+++ server.js
@@ -23,8 +23,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         secure: false, 
-        httpOnly: true,
-        maxAge: 8 * 60 * 60 * 1000 // 8 saat
+        httpOnly: true
     }
 }));
 
